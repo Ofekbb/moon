f3323d3c055fbfae595a60f4f8435e6a68261327
Ofek Ben Baruch <Ofek.benbaruch@surecomp.com>
2023-08-07 22:08:31 +0300

    add routes

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, request
 import requests, os
 
 
-# url_post = os.environ.get('POST_URL', 'https://ofekbb/post')
-url_post = os.environ.get('POST_URL', 'http://backend-svc.default.svc.cluster.local:5001/post')
+# url_post = os.environ.get('POST_URL', 'http://backend-svc.default.svc.cluster.local:5001/post')
+url_post = os.environ.get('POST_URL', 'http://127.0.0.1:5001/post')
+def_api = os.environ.get('DEFUALT_API', 'https://api.chucknorris.io/jokes/random')
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
+def render():
+    return render_template('index.html', def_api=def_api)
+
+@app.route('/', methods=['POST'])
 def post():
     if request.method == 'POST':
         url = request.form['url']
@@ -17,6 +22,34 @@ def post():
         return f'Resonse: {post_response_json}!'
 
     return render_template('index.html')
+
+
+@app.route('/uselessfact', methods=['POST', 'GET'])
+def uselessfact():
+    url = "https://uselessfacts.jsph.pl/api/v2/facts/random"
+    post_response = requests.post(url_post, json=url)
+    post_response_json = post_response.json()
+    post_response_split = post_response_json['text']
+    print("from front",post_response_split)
+    return f'Response: {post_response_split}!'
+
+@app.route('/funnyfact', methods=['POST', 'GET'])
+def funnyfact():
+    url = "https://api.chucknorris.io/jokes/random"
+    post_response = requests.post(url_post, json=url)
+    post_response_json = post_response.json()
+    post_response_split = post_response_json['value']
+    print("from front",post_response_split)
+    return f'Response: {post_response_split}!'
+
+
+
+
+
+# Health check
+@app.route('/ready')
+def health():
+    return 'Im Ready :)', 200
 
 
 if __name__ == '__main__':
